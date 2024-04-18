f7b84e9796e5f3dd1ffe455b9ce6b2eef219dfd8
santiagoruizm01 <160878816+santiagoruizm01@users.noreply.github.com>
2024-04-18 10:44:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if st.button('ON'):
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("cmqtt_s", message)
+    ret= client1.publish("Ruiz", message)
  
     #client1.subscribe("Sensores")
     
@@ -47,7 +47,7 @@ if st.button('OFF'):
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("cmqtt_s", message)
+    ret= client1.publish("Ruiz", message)
   
     
 else:
@@ -61,7 +61,7 @@ if st.button('Enviar valor analógico'):
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("cmqtt_a", message)
+    ret= client1.publish("EAFIT", message)
     
  
 else:
