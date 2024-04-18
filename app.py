4be1126ed20b6a45d5d8c0980220ed2126bb9170
santiagoruizm01 <160878816+santiagoruizm01@users.noreply.github.com>
2024-04-18 10:40:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("GIT-HUB")
+client1= paho.Client("ESPSantiago")
 client1.on_message = on_message
 
 
@@ -29,7 +29,7 @@ st.title("MQTT Control")
 
 if st.button('ON'):
     act1="ON"
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("ESPSantiago")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
@@ -43,7 +43,7 @@ else:
 
 if st.button('OFF'):
     act1="OFF"
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("ESPSantiago")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
@@ -57,7 +57,7 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("ESPSantiago")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
